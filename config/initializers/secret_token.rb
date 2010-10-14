1d53fd76bbd41ea370ac04cc792f46556cb078e2
Shay Friedman <shay.friedman@gmail.com>
2010-10-14 19:29:47 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dizzcussions::Application.config.secret_token = 'c61540a988f2a599ff0f66bc1c6087bcab90658f9036124200afafb0e7391b8d49c2cae9cab0d29e750d0c4aa329745faa8144729ffa9aa137a698ab0999ebd8'
